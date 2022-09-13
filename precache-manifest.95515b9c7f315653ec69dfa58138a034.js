6130205f9c5dd0b693f38a30b5cfabcc8e1aba43
Code Sandbox <hello@codesandbox.io>
2022-09-13 17:46:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.95515b9c7f315653ec69dfa58138a034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6946d16130ffff3b3213210e1a5df09b",
+    "url": "https://.github.io/csb-fxq7lo/index.html"
+  },
+  {
+    "revision": "ab4fbb88c145bb17b993",
+    "url": "https://.github.io/csb-fxq7lo/static/css/main.bec094ea.chunk.css"
+  },
+  {
+    "revision": "6483e8a434a30d6e054b",
+    "url": "https://.github.io/csb-fxq7lo/static/js/2.fc7eb4a0.chunk.js"
+  },
+  {
+    "revision": "ab4fbb88c145bb17b993",
+    "url": "https://.github.io/csb-fxq7lo/static/js/main.3801ea81.chunk.js"
+  },
+  {
+    "revision": "90dd25ca6a291cc3e066",
+    "url": "https://.github.io/csb-fxq7lo/static/js/runtime~main.52fe6f7e.js"
+  }
+]);
